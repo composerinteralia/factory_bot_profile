4a185c1767bfc53b076be3e47da841d99f227ad6
HParker <HParker@github.com>
2022-07-20 10:17:46 -0700

    Add details on child jobs to report
    Updated report looks like:
    
    ++++++++ factory_bot stats:
    
      Spent 2.31 seconds in factory_bot
    
      Factories taking most time overall:
        - :organization factory took 1.61 seconds overall
           (0.81 seconds spent in associated child factories)
    
        - :repository factory took 1.41 seconds overall
           (1.21 seconds spent in associated child factories)
    
        - :user factory took 1.21 seconds overall
           (0.3 seconds spent in associated child factories)
    
        - :profile factory took 0.41 seconds overall
    
      Slowest factories on average:
        - :repository factory took 1.41 seconds on average
          - 0.6 spent in :user called 1/1 time(s)
          - 0.2 spent in :profile called 1/1 time(s)
          - 0.4 spent in :organization called 1/1 time(s)
    
        - :organization factory took 0.8 seconds on average
          - 0.6 spent in :user called 2/2 time(s)
          - 0.2 spent in :profile called 2/2 time(s)
    
        - :user factory took 0.4 seconds on average
          - 0.3 spent in :profile called 3/3 time(s)
    
        - :profile factory took 0.1 seconds on average
    
      Most used factories:
        - :profile factory ran 4 times
    
        - :user factory ran 3 times
    
        - :organization factory ran 2 times
    
        - :repository factory ran 1 times

--- lib/factory_bot_profiler/factory_stat.rb
+++ lib/factory_bot_profiler/factory_stat.rb
@@ -1,18 +1,25 @@
 module FactoryBotProfiler
   class FactoryStat
-    attr_reader :name, :count, :total_time, :total_child_time
+    attr_reader :name, :count, :total_time, :total_child_time, :individual_child_times, :individual_child_count
 
     def initialize(name)
       @name = name
       @count = 0
       @total_time = 0
       @total_child_time = 0
+      @individual_child_times = Hash.new { 0 }
+      @individual_child_count = Hash.new { 0 }
     end
 
     def increment(frame)
       @count += 1
       @total_time += frame.duration
-      @total_child_time += frame.child_time
+      @total_child_time += frame.total_child_time
+
+      frame.child_time.each do |factory_name, time|
+        @individual_child_times[factory_name] += time
+        @individual_child_count[factory_name] += 1
+      end
     end
 
     def average_time
