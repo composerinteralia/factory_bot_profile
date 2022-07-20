b89fbedd5b67f327c3d69b8cbd902b01c05e888b
Daniel Colson <danieljamescolson@gmail.com>
2022-07-20 12:14:00 -0400

    Print to stderr for now

--- lib/factory_bot_profiler/reporters/simple_reporter.rb
+++ lib/factory_bot_profiler/reporters/simple_reporter.rb
@@ -5,28 +5,29 @@ module FactoryBotProfiler
 
       def initialize(collector)
         @collector = collector
+        @io = $stderr
       end
 
       def report
-        puts
-        puts "++++++++ factory_bot stats:"
-        puts
-        puts "  Spent #{(@collector.total_time).round(2)} seconds in factory_bot"
-        puts
-        puts "  Factories taking most time overall:"
+        @io.puts
+        @io.puts "++++++++ factory_bot stats:"
+        @io.puts
+        @io.puts "  Spent #{(@collector.total_time).round(2)} seconds in factory_bot"
+        @io.puts
+        @io.puts "  Factories taking most time overall:"
         @collector.highest_total_time(N).each do |stat|
-          puts "    - :#{stat.name} factory took #{(stat.total_time).round(2)} seconds overall"
-          puts "       (#{stat.total_child_time.round(2)} seconds spent in associated child factories)" unless stat.total_child_time.zero?
+          @io.puts "    - :#{stat.name} factory took #{(stat.total_time).round(2)} seconds overall"
+          @io.puts "       (#{stat.total_child_time.round(2)} seconds spent in associated child factories)" unless stat.total_child_time.zero?
         end
-        puts
-        puts "  Slowest factories on average:"
+        @io.puts
+        @io.puts "  Slowest factories on average:"
         @collector.highest_average_time(N).each do |stat|
-          puts "    - :#{stat.name} factory took #{(stat.average_time).round(2)} seconds on average"
+          @io.puts "    - :#{stat.name} factory took #{(stat.average_time).round(2)} seconds on average"
         end
-        puts
-        puts "  Most used factories:"
+        @io.puts
+        @io.puts "  Most used factories:"
         @collector.highest_count(N).each do |stat|
-          puts "    - :#{stat.name} factory ran #{stat.count} times"
+          @io.puts "    - :#{stat.name} factory ran #{stat.count} times"
         end
       end
     end
