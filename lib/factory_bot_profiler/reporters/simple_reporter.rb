6b2bf6ab8fd287abb01900e6b7e69ea1901bb5c7
Daniel Colson <danieljamescolson@gmail.com>
2022-07-22 10:40:30 -0400

    Show both total and average child times
    This commit takes advantage of 8cc856aa and shows both total and average
    child times.
    
    It also limits to 3 results for the highest total and average times,
    since there's quite a bit of information there now, but 5 for highest
    count.

--- lib/factory_bot_profiler/reporters/simple_reporter.rb
+++ lib/factory_bot_profiler/reporters/simple_reporter.rb
@@ -1,8 +1,6 @@
 module FactoryBotProfiler
   module Reporters
     class SimpleReporter
-      N = 5
-
       def initialize(stats, io: $stdout)
         @stats = stats
         @io = io
@@ -14,26 +12,27 @@ module FactoryBotProfiler
         io.puts
         io.puts "  Spent #{(stats.total_time).round(2)} seconds in factory_bot"
         io.puts
-        io.puts "  Factories taking most time overall:"
-        stats.highest_total_time(N).each do |stat|
-          io.puts "    - :#{stat.name} factory took #{(stat.total_time).round(2)} seconds overall"
-          io.puts "       (#{stat.total_child_time.round(2)} seconds spent in associated child factories)" unless stat.total_child_time.zero?
+        io.puts "  Factories taking the most time overall:"
+        stats.highest_total_time(3).each do |stat|
+          io.puts "    - :#{stat.name} factory took #{(stat.total_time).round(2)} seconds overall (run #{stat.count} times)"
+          stat.child_stats.each do |name, child_stat|
+            io.puts "      - #{child_stat.total_time.round(2)} seconds spent in :#{name} (run #{child_stat.count}/#{stat.count} times)"
+          end
           io.puts
         end
         io.puts
         io.puts "  Slowest factories on average:"
-        stats.highest_average_time(N).each do |stat|
-          io.puts "    - :#{stat.name} factory took #{(stat.average_time).round(2)} seconds on average (called #{stat.count} times)"
+        stats.highest_average_time(3).each do |stat|
+          io.puts "    - :#{stat.name} factory took #{(stat.average_time).round(2)} seconds on average (run #{stat.count} times)"
           stat.child_stats.each do |name, child_stat|
-            io.puts "      - #{child_stat.total_time.round(2)} spent in :#{name} called #{child_stat.count}/#{stat.count} time(s)"
+            io.puts "      - #{child_stat.average_time.round(2)} seconds spent in :#{name} on average (run #{child_stat.count}/#{stat.count} times)"
           end
           io.puts
         end
         io.puts
         io.puts "  Most used factories:"
-        stats.highest_count(N).each do |stat|
+        stats.highest_count(5).each do |stat|
           io.puts "    - :#{stat.name} factory ran #{stat.count} times"
-          io.puts
         end
       end
 
